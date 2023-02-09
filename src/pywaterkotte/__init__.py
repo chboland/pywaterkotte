b355d15d2de5c6b5cfe8fbd7a2cfadac042310d5
Christian Boland <christian@boland-net.de>
2023-02-09 18:30:40 +0100

    make classes available directly in pywaterkotte

--- src/pywaterkotte/__init__.py
+++ src/pywaterkotte/__init__.py
@@ -1,1 +1,11 @@
 name = "pywaterkotte"
+
+from .ecotouch import (
+    AuthenticationException,
+    ConnectionException,
+    Ecotouch,
+    EcotouchTags,
+    InvalidResponseException,
+    InvalidValueException,
+    TagData,
+)
